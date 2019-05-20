5337744b054ff31e419925197e662dfbe34f9934
DingJunyao <dingjunyao0703@163.com>
2019-05-20 10:44:42 +0800

    modify

--- als/als.py
+++ als/als.py
@@ -12,14 +12,16 @@ import time
 import matplotlib.pyplot as plt
 
 
-def s_rate_mean(s_rate):
+def s_rate_equalization(s_rate):
     """
     平均化矩阵内各用户评分
 
     :param s_rate: 用户评分矩阵：pandas.DataFrame
     :return:       平均化后的用户评分矩阵：pandas.DataFrame
+                   各用户的评分的平均值：pandas.Series
     """
-    return s_rate - s_rate.mean()
+    s_rate_mean = s_rate.mean()
+    return (s_rate - s_rate.mean()), s_rate_mean
 
 
 # def cw_sgd(R, k, alpha=0.005, lambda_v=0.005, epoch=0.0001):
@@ -75,6 +77,7 @@ def als(R, k, steps=1000, epoch=0.00001):
     Us.append(U.copy())
     Is.append(I.copy())
     for step in range(steps):
+        print('\r%s' % step, end='', flush=True)
         epu = 0
         epi = 0
         for a in range(U.shape[1]):
@@ -91,7 +94,6 @@ def als(R, k, steps=1000, epoch=0.00001):
         Is.append(I.copy())
         if epu_avg < epoch and epi_avg < epoch:
             break
-        # print('\r%s' % step, end='', flush=True)
     epu = 0
     epi = 0
     for a in range(U.shape[1]):
@@ -131,48 +133,48 @@ def matrix_preparation(s_rate, k, steps=1000, epoch=0.00001):
     return s_rate_predict, Us, Is, epoch_U, epoch_I
 
 
-def recommend_als(s_rate_mean, s_rate_predict, u, num=10):
+def recommend_als(s_rate_equalized, s_rate_predict, u, num=10):
     """
     生成推荐列表
 
-    :param s_rate_mean:    平均化后的用户评分矩阵
-    :param s_rate_predict: 预测用户评分矩阵
-    :param u:              用户ID：整数
-    :param num:            列表内最大项数：整数，默认为10
+    :param s_rate_equalized: 平均化后的用户评分矩阵
+    :param s_rate_predict:   预测用户评分矩阵
+    :param u:                用户ID：整数
+    :param num:              列表内最大项数：整数，默认为10
     :return: 推荐列表：列表，项为元组，元组内项分别为项目ID、项目的评分
     """
     # 选取原评分矩阵中未评分的，且预测值在平均评分及以上的项目
-    r_1 = s_rate_predict.loc[s_rate_mean[u].isnull(), u][
+    r_1 = s_rate_predict.loc[s_rate_equalized[u].isnull(), u][
         s_rate_predict[u] >= 0].sort_values(ascending=False)
     rec_movies = [([i, r_1[i]]) for i in r_1[:num].index]
     return rec_movies
 
 
-def recommend_als_s(s_rate_mean, s_rate_predict, s_similar, u, num=10, similar_limit=0.8):
-    """
-    生成推荐列表
-
-    :param s_rate_mean:    平均化后的用户评分矩阵
-    :param s_rate_predict: 预测用户评分矩阵
-    :param s_similar:      项目相似度矩阵
-    :param u:              用户ID：整数
-    :param num:            列表内最大项数：整数，默认为10
-    :param similar_limit:  相似度的最大值：浮点数，默认为0.8
-    :return: 推荐列表：列表，项为元组，元组内项分别为项目ID、项目的评分
-    """
-    # 选取原评分矩阵中未评分的，且预测值在平均评分及以上的项目
-    r_1 = s_rate_predict.loc[s_rate_mean[u].isnull(), u][
-        s_rate_predict[u] >= 0].sort_values(ascending=False)
-    r_2 = r_1
-    # 选取原评分矩阵中评分的，且预测值在平均评分及以上的项目
-    r_11 = s_rate_predict.loc[~s_rate_mean[u].isnull(), u][
-        s_rate_predict[u] >= 0].sort_values(ascending=False)
-    # 筛选相对于已评分项目，相似度低的项目
-    for j in r_11.index:
-        s_s_1 = s_similar.loc[s_similar.loc[j] < similar_limit, j]
-        r_2 = r_2[r_2.index & s_s_1.index]
-    rec_movies = [([i, r_1[i]]) for i in r_1[r_2.index & r_1.index][:num].index]
-    return rec_movies
+# def recommend_als_s(s_rate_mean, s_rate_predict, s_similar, u, num=10, similar_limit=0.8):
+#     """
+#     生成推荐列表
+#
+#     :param s_rate_mean:    平均化后的用户评分矩阵
+#     :param s_rate_predict: 预测用户评分矩阵
+#     :param s_similar:      项目相似度矩阵
+#     :param u:              用户ID：整数
+#     :param num:            列表内最大项数：整数，默认为10
+#     :param similar_limit:  相似度的最大值：浮点数，默认为0.8
+#     :return: 推荐列表：列表，项为元组，元组内项分别为项目ID、项目的评分
+#     """
+#     # 选取原评分矩阵中未评分的，且预测值在平均评分及以上的项目
+#     r_1 = s_rate_predict.loc[s_rate_mean[u].isnull(), u][
+#         s_rate_predict[u] >= 0].sort_values(ascending=False)
+#     r_2 = r_1
+#     # 选取原评分矩阵中评分的，且预测值在平均评分及以上的项目
+#     r_11 = s_rate_predict.loc[~s_rate_mean[u].isnull(), u][
+#         s_rate_predict[u] >= 0].sort_values(ascending=False)
+#     # 筛选相对于已评分项目，相似度低的项目
+#     for j in r_11.index:
+#         s_s_1 = s_similar.loc[s_similar.loc[j] < similar_limit, j]
+#         r_2 = r_2[r_2.index & s_s_1.index]
+#     rec_movies = [([i, r_1[i]]) for i in r_1[r_2.index & r_1.index][:num].index]
+#     return rec_movies
 
 
 if __name__ == '__main__':
@@ -182,25 +184,81 @@ if __name__ == '__main__':
     EPOCH = 0.0001
     SIMILAR_LIMIT = 0.8
 
-    s_rate = pd.read_csv('../temp/s_rate.csv')
-    s_rate = s_rate.set_index('MovieID')
-    s_rate.rename(columns=int, inplace=True)
+    ML_DS_PATH = '../dataset/ml-out'
+    MATRIX_PATH = '../temp'
 
-    s_similar = pd.read_csv('../temp/s_similar.csv')
+    s_rate_old = pd.read_csv(MATRIX_PATH + '/s_rate_old.csv')
+    s_rate_old = s_rate_old.set_index('MovieID')
+    s_rate_old.rename(columns=int, inplace=True)
+
+    s_similar = pd.read_csv(MATRIX_PATH + '/s_similar.csv')
     s_similar = s_similar.set_index('MovieID')
     s_similar.rename(columns=int, inplace=True)
 
-    s_rate_mean = s_rate_mean(s_rate)
-    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())),
-          '任务开始')
+    s_rate_old_equalized, s_rate_old_mean = s_rate_equalization(s_rate_old)
     model_start = time.time()  # 打点计时
-    s_rate_predict, Us, Is, epoch_U, epoch_I = matrix_preparation(s_rate_mean, K, STEPS, EPOCH)
+    s_rate_predict, Us, Is, epoch_U, epoch_I = matrix_preparation(s_rate_old_equalized, K, STEPS, EPOCH)
     model_end = time.time()  # 打点计时
-    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())),
-          '任务完成')
-    rl = recommend_als(s_rate_mean, s_rate_predict, 1)
-    print(rl)
-    print(model_end - model_start)  # 打点计时
+    recommend_list_example = recommend_als(s_rate_old_equalized, s_rate_predict, 1)
+    print(recommend_list_example)
+    print('Modeling Time: %s' % (model_end - model_start))  # 打点计时
+
+    # RMSE
+    s_rate_new = pd.read_csv(MATRIX_PATH + '/s_rate_new.csv')
+    s_rate_new = s_rate_new.set_index('MovieID')
+    s_rate_new.rename(columns=int, inplace=True)
+
+    s_rate_predict_restore = s_rate_predict + s_rate_old_mean.fillna(0)
+
+    rmse = np.sqrt(((s_rate_new[~s_rate_new.isnull()] - (
+    s_rate_predict_restore[~s_rate_new.isnull()])) ** 2).sum().sum() / (
+                       (~s_rate_new.isnull()).sum().sum()))
+
+    # 准确率 Precision & 召回率 Recall & 覆盖率 Coverage & 多样性 Diversity
+
+    r_and_s_sum = 0
+    r_sum = 0
+    s_sum = 0
+    ru_set = set()
+    sum_diversity_u = 0
+    user_minus = 0
+    rec_time_sum = 0  # 打点计时
+    for u in s_rate_old.columns:
+        print('\r%s' % u, end='', flush=True)
+        select_set = set(s_rate_new[~s_rate_new[u].isnull()][u].index)
+        rec_start = time.time()  # 打点计时
+        recommend_list_with_score = recommend_als(s_rate_old_equalized, s_rate_predict, u)
+        rec_end = time.time()  # 打点计时
+        recommend_list = [i[0] for i in recommend_list_with_score]
+        recommend_set = set(recommend_list)
+        r_and_s_sum += len(recommend_set & select_set)
+        r_sum += len(recommend_set)
+        s_sum += len(select_set)
+        if len(recommend_list) > 1:
+            sum_diversity_u += 1 - (
+                    s_similar.loc[
+                        recommend_list, recommend_list
+                    ].sum().sum() - len(recommend_list)) / (
+                                       0.5 * len(recommend_list) * (
+                                       len(recommend_list) - 1))
+        else:
+            user_minus += 1
+        for i in recommend_list:
+            ru_set.add(i)
+        rec_time_sum += rec_end - rec_start  # 打点计时
+    coverage = len(ru_set) / len(s_rate_old.index)
+    diversity = sum_diversity_u / (len(s_rate_old.columns) - user_minus)
+    rec_time = rec_time_sum / len(s_rate_old.columns)  # 打点计时
+    precision = r_and_s_sum / r_sum
+    recall = r_and_s_sum / s_sum
+    f_measure = (2 * precision * recall) / (precision + recall)
+    print('Average Recommend Time: %s' % rec_time)  # 打点计时
+    print('RMSE: %s' % rmse)
+    print('Precision: %s' % precision)
+    print('Recall: %s' % recall)
+    print('F-Measure: %s' % f_measure)
+    print('Coverage: %s' % coverage)
+    print('Diversity: %s' % diversity)
 
     # plt.plot(list(range(len(epoch_U))), epoch_U, color='r')
     # plt.plot(list(range(len(epoch_U))), epoch_I, color='g')
@@ -261,36 +319,39 @@ if __name__ == '__main__':
     # plt.xlabel('Steps')
     # plt.legend()
     # plt.show()
-    rmse = np.sqrt(
-        ((s_rate_mean.fillna(0) - s_rate_predict[
-            ~s_rate_mean.isnull()]) ** 2).sum().sum() / (
-            s_rate_mean.count().count()))
-    print('RMSE: %s' % rmse)
 
-    # 覆盖率 Coverage & 多样性 Diversity
-    ru_set = set()
-    sum_diversity_u = 0
-    rec_time_sum = 0  # 打点计时
-    for u in s_rate.columns:
-        rec_start = time.time()  # 打点计时
-        # recommend_list_with_score = recommend_svd(s_rate_mean, s_rate_predict, u)
-        recommend_list_with_score = recommend_als(s_rate_mean, s_rate_predict, u, RECOMMEND_NUM)
-        rec_end = time.time()  # 打点计时
-        recommend_list = [i[0] for i in recommend_list_with_score]
-        sum_diversity_u += 1 - (s_similar.loc[recommend_list, recommend_list
-                                ].sum().sum() - len(
-            recommend_list)) / (0.5 * len(recommend_list) * (
-                len(recommend_list) - 1))
-        for i in recommend_list:
-            ru_set.add(i)
-        rec_time_sum += rec_end - rec_start  # 打点计时
-        print('\r%s' % u, end='', flush=True)
-    coverage = len(ru_set) / len(s_rate.index)
-    diversity = sum_diversity_u / len(s_rate.columns)
-    rec_time = rec_time_sum / len(s_rate.columns)  # 打点计时
-    print('Coverage: %s' % coverage)
-    print('Diversity: %s' % diversity)
-    print('Time: %s' % rec_time)  # 打点计时
+
+
+    # rmse = np.sqrt(
+    #     ((s_rate_mean.fillna(0) - s_rate_predict[
+    #         ~s_rate_mean.isnull()]) ** 2).sum().sum() / (
+    #         s_rate_mean.count().count()))
+    # print('RMSE: %s' % rmse)
+    #
+    # # 覆盖率 Coverage & 多样性 Diversity
+    # ru_set = set()
+    # sum_diversity_u = 0
+    # rec_time_sum = 0  # 打点计时
+    # for u in s_rate.columns:
+    #     rec_start = time.time()  # 打点计时
+    #     # recommend_list_with_score = recommend_svd(s_rate_mean, s_rate_predict, u)
+    #     recommend_list_with_score = recommend_als(s_rate_mean, s_rate_predict, u, RECOMMEND_NUM)
+    #     rec_end = time.time()  # 打点计时
+    #     recommend_list = [i[0] for i in recommend_list_with_score]
+    #     sum_diversity_u += 1 - (s_similar.loc[recommend_list, recommend_list
+    #                             ].sum().sum() - len(
+    #         recommend_list)) / (0.5 * len(recommend_list) * (
+    #             len(recommend_list) - 1))
+    #     for i in recommend_list:
+    #         ru_set.add(i)
+    #     rec_time_sum += rec_end - rec_start  # 打点计时
+    #     print('\r%s' % u, end='', flush=True)
+    # coverage = len(ru_set) / len(s_rate.index)
+    # diversity = sum_diversity_u / len(s_rate.columns)
+    # rec_time = rec_time_sum / len(s_rate.columns)  # 打点计时
+    # print('Coverage: %s' % coverage)
+    # print('Diversity: %s' % diversity)
+    # print('Time: %s' % rec_time)  # 打点计时
 
     # on 1/10 dataset(k=40, 1000)
     # RMSE: 2.054283044022762
--- svd/svd.py
+++ svd/svd.py
@@ -46,18 +46,18 @@ def matrix_prepare(s_rate_equalized, energy_ratio=0.8):
     return s_rate_predict
 
 
-def recommend_svd(s_rate_equalizated, s_rate_predict, u, num=10):
+def recommend_svd(s_rate_equalized, s_rate_predict, u, num=10):
     """
     生成推荐列表
 
-    :param s_rate_equalizated: 平均化后的用户评分矩阵
+    :param s_rate_equalized: 平均化后的用户评分矩阵
     :param s_rate_predict:     预测用户评分矩阵
     :param u:                  用户ID：整数
     :param num:                列表内最大项数：整数，默认为10
     :return: 推荐列表：列表，项为元组，元组内项分别为项目ID、项目的评分
     """
     # 选取原评分矩阵中未评分的，且预测值在平均评分及以上的项目
-    r_1 = s_rate_predict.loc[s_rate_equalizated[u].isnull(), u][
+    r_1 = s_rate_predict.loc[s_rate_equalized[u].isnull(), u][
         s_rate_predict[u] >= 0].sort_values(ascending=False)
     rec_movies = [([i, r_1[i]]) for i in r_1[:num].index]
     return rec_movies
